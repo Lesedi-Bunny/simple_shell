f33e485c74226452f430a8487f5e422fc8c977ef
thabimt <shazzyzuma@yahoo.com>
2023-12-13 18:02:33 -0800

    env

--- shell_env.c
+++ shell_env.c
@@ -8,8 +8,6 @@
  * Description: This function prints the current environment variables
  * to the standard output without using the `printf` function
  *
- * @argc: The number of command-line arguments.
- * @argv: An array of pointers to the command-line argument.
  * @envp: The environment variables as a null-terminated array of strings.
  * Return: The exit status of the program.
  */
@@ -19,26 +17,16 @@ void env_builtin(char *envp[])
 
 	while (*env_ptr != NULL)
 	{
-		char *env_var = *env_ptr;
-		char *ptr = env_var;
+	char *env_var = *env_ptr;
+	char *ptr = env_var;
 
-		while (*ptr != '\0')
+	while (*ptr != '\0')
 	{
 		write(STDOUT_FILENO, ptr, 1);
 		ptr++;
 	}
 
-		write(STDOUT_FILENO, "\n", 1);
-		env_ptr++;
+	write(STDOUT_FILENO, "\n", 1);
+	env_ptr++;
 	}
-}
-
-int main(int argc, char *argv[], char *envp[])
-{
-	(void)argc;  /* Silence unused parameter warning */
-	(void)argv;  /* Silence unused parameter warning */
-
-	env_builtin(envp);
-
-	return (0);
 }
